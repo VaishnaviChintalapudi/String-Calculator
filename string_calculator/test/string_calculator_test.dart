78d3c3cc4f7dacea365463d58ff1d414d5609a3c
VaishnaviChintalapudi <vaishnavi.chinthalapudi@gmail.com>
2025-06-28 09:57:49 +0530

    Added a testcase that returns the number itself if only one provided

--- string_calculator/test/string_calculator_test.dart
+++ string_calculator/test/string_calculator_test.dart
@@ -6,4 +6,8 @@ void main() {
   test("Returns 0 for empty string", () {
     expect(addNumbers(""), 0);
   });
+
+  test("Returns the number if only one is provided", () {
+    expect(addNumbers("10"), 10);
+  });
 }
